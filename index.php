44be2bcd6ca988e6b2b327608c8776455a472603
heber <feanorkun@hotmail.com>
2016-08-11 20:38:25 +0000

    boletosimples.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once 'vendor/autoload.php';
 
 BoletoSimples::configure(array(
-  "environment" => 'production', // default: 'sandbox'
-  "access_token" => 'access-token'
+  "environment" => 'sandbox', 
+  "access_token" => 'a6e3e7990d39c413862d7fcc126f57c418d7cf6dbf18e2da8eb3dea738a17349'
 ));
 
 use Phalcon\Mvc\Micro;
@@ -12,11 +12,7 @@ use Phalcon\Mvc\Micro;
 $app = new Micro();
 
 $app->get('/', function () {
-    echo 'home';
-});
-
-$app->get('/api/test/{data}', function ($data) {
-    echo $data;
+    echo 'bswebhook!';
 });
 
 $app->post('/bswebhook/', function ($data) {
