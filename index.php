6c08554c044ec33429391d5bcb133447716e024a
heber <feanorkun@hotmail.com>
2016-08-11 18:55:26 +0000

    starting phalcon

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php print_r(get_loaded_extensions()); ?>
+<?php
+
+use Phalcon\Mvc\Micro;
+
+$app = new Micro();
+
+// Retrieves all robots
+$app->get('/api/robots', function () {
+
+});
+
+// Searches for robots with $name in their name
+$app->get('/api/robots/search/{name}', function ($name) {
+
+});
+
+// Retrieves robots based on primary key
+$app->get('/api/robots/{id:[0-9]+}', function ($id) {
+
+});
+
+// Adds a new robot
+$app->post('/api/robots', function () {
+
+});
+
+// Updates robots based on primary key
+$app->put('/api/robots/{id:[0-9]+}', function () {
+
+});
+
+// Deletes robots based on primary key
+$app->delete('/api/robots/{id:[0-9]+}', function () {
+
+});
+
+$app->handle();
