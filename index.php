35e91c6a4573a20c294e707e1918f198ce817721
heber <feanorkun@hotmail.com>
2016-08-12 18:37:45 +0000

    boletosimples.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-BoletoSimples::configure(array(
-  "environment" => 'sandbox', 
-  "access_token" => 'a6e3e7990d39c413862d7fcc126f57c418d7cf6dbf18e2da8eb3dea738a17349'
-));
-
 use Phalcon\Mvc\Micro;
 
 $app = new Micro();
