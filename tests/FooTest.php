d196e3937678b41ff5b2f32680e2dcb6ea01aea2
heber <feanorkun@hotmail.com>
2016-08-15 17:00:06 +0000

    tests

--- tests/FooTest.php
+++ tests/FooTest.php
@@ -1,15 +1,9 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-
 class FooTest extends TestCase
 {
-    
     public function testCanBeNegated()
     {
-        
         $this->assertEquals(-1, -1);
     }
-
-    
 }
