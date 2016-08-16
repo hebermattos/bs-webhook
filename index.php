c69b3b5ae8021c62bcea08a7a609cbe67301f491
heber <feanorkun@hotmail.com>
2016-08-16 17:10:09 +0000

    latin chars

--- index.php
+++ index.php
@@ -18,7 +18,9 @@ $app->post('/bswebhook', function () use ($app) {
     $request = new \Phalcon\Http\Request();
     $header = $request->getHeader('HTTP_X_HUB_SIGNATURE');
 
-    $body =  uft8_decode(str_replace("\\", "", json_encode($app->request->getJsonRawBody())));
+    //$body =  uft8_decode(str_replace("\\", "", json_encode($app->request->getJsonRawBody())));
+    
+    $body = $request->getRawBody();
 
     $hashedBody = hash_hmac('sha1', $body, 'a6e3e7990d39c413862d7fcc126f57c418d7cf6dbf18e2da8eb3dea738a17349');
     
