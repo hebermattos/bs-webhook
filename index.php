0e502e1a5935f2251414196e43340bf7961240c5
heber <feanorkun@hotmail.com>
2016-08-17 20:42:37 +0000

    test

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $app->post('/bswebhook', function () use ($app) {
     
     $request = new Request();
     
-    $allheaders = "h: ".$request->getHeaders();
+    $allheaders = $request->getHeaders();
     
     $header = $request->getHeader('HTTP_X_HUB_SIGNATURE');
     $rawBody = $request->getRawBody();
