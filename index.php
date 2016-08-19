e66a3f264856e58cd18852e740eb642761d03f3c
heber <feanorkun@hotmail.com>
2016-08-19 14:11:05 +0000

    route handle

--- index.php
+++ index.php
@@ -16,40 +16,41 @@ $di = ContainerBuilder::Build();
 $app = new Micro();
 $app->setDI($di);
 
+$app->before(function () use ($app) {
+    
+    $header = $app->request->getHeader('HTTP_X_HUB_SIGNATURE');
+    $rawBody = $app->request->getRawBody();
+    $hashedBody = hash_hmac('sha1', $rawBody, $app->config->environment->secret);
+    
+    //$code = "401";
+    //$status = "NOT AUTHORIZED";
+    
+    return strcmp($header, 'sha1='.$hashedBody);
+});
+
 $app->get('/', function ()  {
     echo 'bswebhook';
 });
 
 $app->post('/bswebhook', function () use ($app) {
     
-    $header = $app->request->getHeader('HTTP_X_HUB_SIGNATURE');
-    $rawBody = $app->request->getRawBody();
-    $hashedBody = hash_hmac('sha1', $rawBody, $app->config->environment->secret);
-    
     $response = new Response();
     $response->setContentType('application/json');
     $code = "200";
     $status = 'OK';
     $data = NULL;
-    
-    if(strcmp($header, 'sha1='.$hashedBody) == 0)
-    {
-        try {
-            $options = ['json' => $app->request->getJsonRawBody(),  'Authorization' => ['Basic '.$app->config->environment->token] ];
-            $data = $app->client->request('POST', $app->config->environment->url, $options)->getBody()->getContents();
-        } catch (ServerException $e) {
-            $code = 500;
-            $status = "INTERNAL SERVER ERROR";
-            $data =  Psr7\str($e->getResponse());
-        } catch (ClientException $e) {
-            $code = 400;
-            $status = "BAD REQUEST";
-            $data =  Psr7\str($e->getResponse());
-        }
-    }
-    else {
-        $code = "401";
-        $status = "NOT AUTHORIZED";
+   
+    try {
+        $options = ['json' => $app->request->getJsonRawBody(),  'Authorization' => ['Basic '.$app->config->environment->token] ];
+        $data = $app->client->request('POST', $app->config->environment->url, $options)->getBody()->getContents();
+    } catch (ServerException $e) {
+        $code = 500;
+        $status = "INTERNAL SERVER ERROR";
+        $data =  Psr7\str($e->getResponse());
+    } catch (ClientException $e) {
+        $code = 400;
+        $status = "BAD REQUEST";
+        $data =  Psr7\str($e->getResponse());
     }
     
     $response->setJsonContent(
