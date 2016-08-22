e5b75f695761182118aad4e0a918e5f38299a1f9
heber <feanorkun@hotmail.com>
2016-08-22 22:43:00 +0000

    async post

--- index.php
+++ index.php
@@ -42,20 +42,22 @@ $app->post('/bswebhook', function () use ($app) {
  
     $options = ['json' => $app->request->getJsonRawBody(),  'Authorization' => ['Basic '.$app->config->environment->token] ];
     $promise = $app->client->requestAsync('POST', $app->config->environment->url, $options);
+     
+    $result = $app->response;
                   
     $promise->then(
         function (ResponseInterface $response) {
-            $app->response->setJsonContent(array('status' => 'OK','data' => $response->getBody()->getContents()));
-            $app->response->setStatusCode(200);
-            return $app->response;
+            $result->setJsonContent(array('status' => 'OK','data' => $response->getBody()->getContents()));
+            $result->setStatusCode(200);
+            return $result;
         },
         function (RequestException  $e) {
-            $app->response->setJsonContent(array('status' => 'ERRO','data' => $e->getResponse()->getBody()->getContents()));
-            $app->response->setStatusCode(500);
-            return $app->response;          
+            $result->setJsonContent(array('status' => 'ERRO','data' => $e->getResponse()->getBody()->getContents()));
+            $result->setStatusCode(500);
+            return $result;          
         });
         
-     $promise->wait();
+    $promise->wait();
 });
 
 $app->get('/', function () {
