c118e57f89dee1137ee031e65c415da85c71393f
heber <feanorkun@hotmail.com>
2016-08-23 17:09:52 +0000

    valid token test

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -55,7 +55,7 @@ class FeatureContext implements Context, SnippetAcceptingContext
             $requestResponse = $this->client->request('POST', 'http://rcbswebhook-com.umbler.net/index.php?_url=/bswebhook',  [
                     'json' => $this->validPayload,
                     'headers'  => [
-                        'X-Hub-Signature' => $this->token
+                        'HTTP_X_HUB_SIGNATURE' => $this->token
                     ]
                 ]
             );
